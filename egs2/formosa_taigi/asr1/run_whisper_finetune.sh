d5ab0eb10aaa67657c6e85191c7a7df5bdd7691d
chocho0130 <jerry38370198@gmail.com>
2024-04-30 13:21:23 +0000

    run_whisper_finetune.sh add whisper small

--- egs2/formosa_taigi/asr1/run_whisper_finetune.sh
+++ egs2/formosa_taigi/asr1/run_whisper_finetune.sh
@@ -9,7 +9,8 @@ train_set=train
 valid_set=dev
 test_sets="dev test"
 
-asr_config=conf/tuning/train_asr_whisper_medium_finetune.yaml
+#asr_config=conf/tuning/train_asr_whisper_medium_finetune.yaml
+asr_config=conf/tuning/train_sot_asr_whisper_small.yaml
 #asr_config=conf/tuning/train_asr_whisper_medium_lora_finetune.yaml
 #sr_config=cconf/tuning/train_asr_whisper_large_lora_finetune.yaml
 inference_config=conf/tuning/decode_asr_whisper_noctc_beam10.yaml
@@ -46,4 +47,4 @@ speed_perturb_factors="0.9 1.0 1.1"
     --asr_text_fold_length 150 \
     --lm_fold_length 150 \
     --lm_train_text "data/${train_set}/text" "$@" \
-    --asr_args "--max_epoch 100"
+    --asr_args "--max_epoch 1"
